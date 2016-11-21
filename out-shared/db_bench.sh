e60aa33b7b910b13d574154f36e0642dfc509fdd
mingfireyu <mingfire@mail.ustc.edu.cn>
2016-11-21 21:32:21 +0800

    add db_bench.sh

---
+++ out-shared/db_bench.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+totalsize=$(expr 1024 \* 1024 \* 1024 \* 10)
+totalsize=$(expr 1024 \* 1024)
+benchmarks=fillseq,fillrandom
+value_size=$[64]
+let num=totalsize/value_size
+bloom_bits=10
+log_open=1
+for i in `seq 1 3`;
+do 
+    time ./db_bench --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open"  > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
+    log_open=0
+    time ./db_bench --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open" > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
+    log_open=1
+    let value_size=value_size*16
+    let num=totalsize/value_size
+done
+#
