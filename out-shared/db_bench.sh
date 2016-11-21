bd24eea61906e193ce1af289561ac666c603fda4
mingfireyu <mingfire@mail.ustc.edu.cn>
2016-11-21 21:47:41 +0800

    add db

--- out-shared/db_bench.sh
+++ out-shared/db_bench.sh
@@ -6,11 +6,12 @@ value_size=$[64]
 let num=totalsize/value_size
 bloom_bits=10
 log_open=1
+db=/home/ming/testdb
 for i in `seq 1 3`;
 do 
-    time ./db_bench --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open"  > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
+    time ./db_bench --db="$db" --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open"  > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
     log_open=0
-    time ./db_bench --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open" > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
+    time ./db_bench --db="$db" --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open" > bench_result_"$value_size"B_"$num"_l"$log_open" 2>&1
     log_open=1
     let value_size=value_size*16
     let num=totalsize/value_size
