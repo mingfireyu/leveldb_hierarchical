cf8d0de5c105cb966420d3f4d17b15cb0bc50711
mingfireyu <mingfire@mail.ustc.edu.cn>
2017-02-14 05:10:54 +0800

    add db_bench.sh

---
+++ db_bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#totalsize=$(expr 1024 \* 1024 \* 1024 \* 1)
+totalsize=$(expr 1024 \* 1024 \* 100)
+benchmarks=fillrandom,stats,readrandom,stats
+value_size=64
+let num=totalsize/value_size
+bloom_bits=10
+log_open=1
+db=/home/ming/testdb
+# for i in `seq 1 3`;
+# do 
+#     time ./db_bench --db="$db" --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open"  > bench_result_"$value_size"B_"$num"_l"$log_open" 
+#     log_open=0
+#     time ./db_bench --db="$db" --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open" > bench_result_"$value_size"B_"$num"_l"$log_open" 
+#     log_open=1
+#     let value_size=value_size*16
+#     let num=totalsize/value_size
+# done
+#
+
+time out-shared/db_bench --db="$db" --benchmarks="$benchmarks" --value_size="$value_size" --num="$num" --bloom_bits="$bloom_bits" --log_open="$log_open" --max_file_size=4194304 > bench_result_"$value_size"B_"$num"_l"$log_open" 
